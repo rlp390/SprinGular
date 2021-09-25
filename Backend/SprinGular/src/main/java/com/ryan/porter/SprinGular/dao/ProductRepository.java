d6dcaedd5ee7db3eeb4348408da4c5fe84db5733
rlp390 <rlp390@hotmail.com>
2021-09-25 15:13:54 -0500

    Added call to backend to retrieve products
    Added call to backend to retrieve products

--- Backend/SprinGular/src/main/java/com/ryan/porter/SprinGular/dao/ProductRepository.java
+++ Backend/SprinGular/src/main/java/com/ryan/porter/SprinGular/dao/ProductRepository.java
@@ -2,7 +2,8 @@ package com.ryan.porter.SprinGular.dao;
 
 import com.ryan.porter.SprinGular.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
-
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
